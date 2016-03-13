8db6a9450d167d5c6b3c75306c7f179cfc88e3e7
Price Matthews <p0matt001@gmail.com>
2016-03-13 13:03:07 -0400

    Removed angular function added ngAnimate to button

--- JavaScript/Angular.js
+++ JavaScript/Angular.js
@@ -1,8 +1,8 @@
-angular.module("skateApp", [])
+angular.module("skateApp", ['ngAnimate'])
 angular.module('skateApp').config(['$controllerProvider', function($controllerProvider) {
   $controllerProvider.allowGlobals();
 }]);
 
-function CountController($scope) {
-	$scope.count = function() { return 12; }
-}
+
+
+
