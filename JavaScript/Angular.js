307b01f4223fe8a49b5583916af8dada6c1e702b
p0matt01 <p0matt001@gmail.com>
2016-03-14 19:02:24 -0400

    Possible first commit

--- JavaScript/Angular.js
+++ JavaScript/Angular.js
@@ -3,6 +3,26 @@ angular.module('skateApp').config(['$controllerProvider', function($controllerPr
   $controllerProvider.allowGlobals();
 }]);
 
+angular.module('skateApp')
+  .value('tricks', {points: 0});
+
+
+angular.module('skateApp')
+  .controller('TrickController', function($scope, tricks) {
+    $scope.tricks = tricks;
+    $scope.increment = function() {
+      $scope.tricks.points++;
+    };
+  });
 
 
 
+
+/* ===========================
+Date
+=============================*/
+
+angular.module('skateApp')
+  .controller('DateController', function($scope) {
+    $scope.now = Date.now();
+  });
